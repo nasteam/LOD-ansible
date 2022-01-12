54f964393f61c0eec58fee6064f0afd4a91ddb07
iwishiwasgay <iwishiwasgay@users.noreply.github.com>
2022-01-12 17:15:11 -0600

    Update cifs01.sh

--- restable/cifs01.sh
+++ restable/cifs01.sh
@@ -1,1 +1,2 @@
-curl -X POST /stuff/to/be/determined
+# curl -X POST /stuff/to/be/determined
+curl -X GET "https://192.168.0.111/api/cluster/software?return_records=true&return_timeout=15&fields=version" --include --insecure --user admin:Netapp1!
