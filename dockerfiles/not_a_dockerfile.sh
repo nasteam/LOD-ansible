70d258b212579695953b5b1579ba7494c74af178
iwishiwasgay <iwishiwasgay@users.noreply.github.com>
2022-02-01 14:40:18 -0600

    Update and rename not_a_dockerfile to not_a_dockerfile.sh

---
+++ dockerfiles/not_a_dockerfile.sh
@@ -0,0 +1,27 @@
+#! /bin/sh
+# literally don't know how dockerfiles work
+# even if i did, this new lab is so weird
+set -e
+trap 'lmao it broke' ERR
+
+ansible-galaxy collection install community.windows
+ansible-galaxy collection install ansible.windows
+ansible-galaxy collection install ansible.posix
+ansible-galaxy collection install netapp.ontap:21.14.1
+
+ssh-keygen -t rsa -q -f "$HOME/.ssh/id_rsa" -N ""
+sshpass -p Netapp1! ssh-copy-id 192.168.0.188
+sshpass -p Netapp1! scp -o StrictHostKeyChecking=no ~/.ssh/id_rsa.pub administrator@demo@dc1.demo.netapp.com:C:\\ProgramData\\ssh\\administrators_authorized_keys
+sshpass -p Netapp1! scp -o StrictHostKeyChecking=no ~/.ssh/id_rsa.pub administrator@demo@jumphost.demo.netapp.com:C:\\ProgramData\\ssh\\administrators_authorized_keys
+sshpass -p Netapp1! scp -o StrictHostKeyChecking=no ~/.ssh/id_rsa.pub root@192.168.0.62:~/.ssh/authorized_keys
+
+sshpass -p Netapp1! ssh -o StrictHostKeyChecking=no administrator@demo@dc1.demo.netapp.com "get-acl C:\\ProgramData\\ssh\\ssh_host_dsa_key | set-acl C:\\ProgramData\\ssh\\administrators_authorized_keys"
+sshpass -p Netapp1! ssh -o StrictHostKeyChecking=no administrator@demo@jumphost.demo.netapp.com "get-acl C:\\ProgramData\\ssh\\ssh_host_dsa_key | set-acl C:\\ProgramData\\ssh\\administrators_authorized_keys" 
+
+echo "DC1.demo.netapp.com ansible_connection=ssh ansible_user=administrator@demo ansible_shell_type=powershell" >> /etc/ansible/hosts
+echo "jumphost.demo.netapp.com ansible_connection=ssh ansible_user=administrator@demo ansible_shell_type=powershell" >> /etc/ansible/hosts
+echo "192.168.0.62 ansible_connection=ssh anisble_user=root" >> /etc/ansible/hosts
+
+curl -L -o playbook.yml "https://raw.githubusercontent.com/nasteam/LOD-ansible/main/new-playbooks/$playbook.yml"
+
+ansible-playbook playbook.yml
