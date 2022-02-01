cf4d23b1c33b2d870a0aabb459fa6d0d886507e9
iwishiwasgay <iwishiwasgay@users.noreply.github.com>
2022-02-01 15:08:09 -0600

    Update not_a_dockerfile.sh
    TO-DO: add variable thing for $playbook.yml

--- dockerfiles/not_a_dockerfile.sh
+++ dockerfiles/not_a_dockerfile.sh
@@ -10,10 +10,10 @@ ansible-galaxy collection install ansible.posix
 ansible-galaxy collection install netapp.ontap:21.14.1
 
 ssh-keygen -t rsa -q -f "$HOME/.ssh/id_rsa" -N ""
-sshpass -p Netapp1! ssh-copy-id 192.168.0.188
+sshpass -p Netapp1! ssh-copy-id 192.168.0.4
 sshpass -p Netapp1! scp -o StrictHostKeyChecking=no ~/.ssh/id_rsa.pub administrator@demo@dc1.demo.netapp.com:C:\\ProgramData\\ssh\\administrators_authorized_keys
 sshpass -p Netapp1! scp -o StrictHostKeyChecking=no ~/.ssh/id_rsa.pub administrator@demo@jumphost.demo.netapp.com:C:\\ProgramData\\ssh\\administrators_authorized_keys
-sshpass -p Netapp1! scp -o StrictHostKeyChecking=no ~/.ssh/id_rsa.pub root@192.168.0.62:~/.ssh/authorized_keys
+sshpass -p Netapp1! scp -o StrictHostKeyChecking=no ~/.ssh/id_rsa.pub root@192.168.0.61:~/.ssh/authorized_keys
 
 sshpass -p Netapp1! ssh -o StrictHostKeyChecking=no administrator@demo@dc1.demo.netapp.com "get-acl C:\\ProgramData\\ssh\\ssh_host_dsa_key | set-acl C:\\ProgramData\\ssh\\administrators_authorized_keys"
 sshpass -p Netapp1! ssh -o StrictHostKeyChecking=no administrator@demo@jumphost.demo.netapp.com "get-acl C:\\ProgramData\\ssh\\ssh_host_dsa_key | set-acl C:\\ProgramData\\ssh\\administrators_authorized_keys" 
@@ -22,6 +22,4 @@ echo "DC1.demo.netapp.com ansible_connection=ssh ansible_user=administrator@demo
 echo "jumphost.demo.netapp.com ansible_connection=ssh ansible_user=administrator@demo ansible_shell_type=powershell" >> /etc/ansible/hosts
 echo "192.168.0.61 ansible_connection=ssh anisble_user=root" >> /etc/ansible/hosts
 
-curl -L -o playbook.yml "https://raw.githubusercontent.com/nasteam/LOD-ansible/main/new-playbooks/$playbook.yml"
-
-ansible-playbook playbook.yml
+ansible-pull -U https://raw.githubusercontent.com/nasteam/LOD-ansible/main/new-playbooks/$playbook.yml
