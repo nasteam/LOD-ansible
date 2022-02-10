0cd67de043553df92e8b548230d0f85adb5ebd7f
Jason Townsend <jtownsen@netapp.com>
2022-02-10 10:23:32 -0600

    adding pip line for pexpect

--- dockerfiles/playbook-selector.sh
+++ dockerfiles/playbook-selector.sh
@@ -22,6 +22,9 @@ if  ! test -f "$HOME/.ssh/id_rsa"
         ansible-galaxy collection install netapp.ontap:21.14.1
         ansible-galaxy collection install ogratwicklcs.realmd_ipa_ad
 
+        #realm join needs pexpect to send password
+        pip install pexpect
+
         # prep ssh keys
         ssh-keygen -t rsa -q -f "$HOME/.ssh/id_rsa" -N ""
         sshpass -p Netapp1! ssh-copy-id -o StrictHostKeyChecking=no 192.168.0.61
